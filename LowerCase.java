81ca6eb29dad3d72397060bdc243fcd98876dd29
Nadav Meroz <154219703+Nadav-gif@users.noreply.github.com>
2024-02-04 11:21:08 +0200

    Update LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -1,17 +1,15 @@
-/** String processing exercise 1. */
 public class LowerCase {
-    public static void main(String[] args) {  
-        String str = args[0];
-        System.out.println(lowerCase(str));
-    }
-
-   /**
-    * Returns a string which is identical to the original string, 
-    * except that all the upper-case letters are converted to lower-case letters.
-    * Non-letter characters are left as is.
-    */
-    public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+    public static void main(String[] args) {
+        String s = args[0];
+        String new_s = "";
+        char c = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) > 64 && s.charAt(i) < 91) {
+                new_s = new_s + (char)(s.charAt(i) + 32);
+            } else {
+                new_s = new_s + (char)(s.charAt(i));
+            }
+        }
+        System.out.println(new_s);
     }
 }
